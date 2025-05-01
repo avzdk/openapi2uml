e54e53fdb6211e65dd8e3a347a447a74722ba5a4
Allan Vestergaard <info@avz.dk>
2025-05-01 09:11:38 +0200

    Argumentparser

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import argparse
 from modules.uml_generator import UMLGenerator
 
 if __name__ == "__main__":
-    schema_dir = "./data/schemas"
-    uml_generator = UMLGenerator(schema_dir)
+    parser = argparse.ArgumentParser(description="Generate UML diagrams from OpenAPI schemas.")
+    parser.add_argument("schema_dir", type=str, help="Path to the directory containing OpenAPI schemas.")
+    args = parser.parse_args()
+
+    uml_generator = UMLGenerator(args.schema_dir)
     uml_generator.generate_uml()
